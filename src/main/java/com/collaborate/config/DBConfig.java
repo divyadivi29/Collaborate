483d1196ac4e94bf4baf65dc0188e784a7d2d18d
SONY <SONY@SONY-PC>
2017-09-08 16:07:51 +0530

    DBConfig

--- src/main/java/com/collaborate/config/DBConfig.java
+++ src/main/java/com/collaborate/config/DBConfig.java
@@ -4,8 +4,10 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 
 public class DBConfig
@@ -14,28 +16,32 @@ public class DBConfig
 public DataSource getOracleDataSource()
 {
 	DriverManagerDataSource driverManagerDataSource=new DriverManagerDataSource();
-	driverManagerDataSource.setDriverClassName("");
-	driverManagerDataSource.setUrl("");
-	driverManagerDataSource.setUsername("");
-	driverManagerDataSource.setPassword("");
+	driverManagerDataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+	driverManagerDataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+	driverManagerDataSource.setUsername("hr");
+	driverManagerDataSource.setPassword("hr");
 	return driverManagerDataSource;
 }
 public Properties getHibernateProperties()
 {
 	Properties properties=new Properties();
-	properties.setProperty("","");
+	
 	properties.put("","");
 	return properties;
 }
 @Bean
- public Sessionfactory getSessionFactory()
+ public SessionFactory getSessionFactory()
  {
-	LocalSessionFactoryBuilder localSessionFactoryBuilder=new LocalSessionFactoryBuilder();
-	LocalSessionFactoryBuilder.addProperties(getHibernateProperties);
-	LocalSessionFactoryBuilder.addAnnotatedclass(blog.class);
+	LocalSessionFactoryBuilder localSessionFactoryBuilder=new LocalSessionFactoryBuilder(getOracleDataSource());
+	localSessionFactoryBuilder.addProperties(getHibernateProperties());
+	
 	System.out.println("SessionFactory Bean created");
-	return LocalSessionFactoryBuilder.buildSessionFactory();
-	
-	
+	return localSessionFactoryBuilder.buildSessionFactory();
  }
+/*@Bean
+public HibernateTransactionManager getHibernateTransactionManager(SessionFactory)
+{
+	
+}*/
+ 
 }
